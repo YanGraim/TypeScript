d298e3e2295fb58fb1d57dbd4e4fff650936cd9c
YanGraim <yangraim@gmail.com>
2025-01-09 14:57:18 -0300

    Modificadores: protected

--- classes/build/heranca.js
+++ classes/build/heranca.js
@@ -5,25 +5,38 @@
 */
 /*
     4 MODIFICADORES: Public, Private, Protected, readonly
+
+    >> Protected: Atributos e métodos protegidos podem ser acessados ou alterados por meio da
+     classe em que foram criados e por meio das classes que forem filhas (que extende a classe pai).
+
+    >> Private: Podem ser acessados ou alterados apenas por meio da classe em que foram criados,
+     ou seja ele nao pode ser acessado ou modificado fora da classe em que foi criado.
+
+    >> Readonly: (apenas para atributos) Podemos apenas ler e ver esse atributo porem
+     nao podemos alterar ele.
 */
 class Usuario {
-    constructor(nome, email) {
+    constructor(id, nome, email) {
+        this.id = id;
         this.nome = nome;
         this.email = email;
     }
 }
 class Admin extends Usuario {
-    constructor(nome, email, cargo, nivel) {
+    constructor(id, nome, email, cargo, nivel) {
         // Chamando o construtor da classe pai
-        super(nome, email);
+        super(id, nome, email);
         this.cargo = cargo;
         this.nivel = nivel;
     }
-    mudarCargo() {
-        console.log("Alterando cargo");
+    mudarCargo(cargo) {
+        console.log("Alterando cargo para:", cargo);
+        console.group("Id do usuário:", this.id);
+    }
+    acessarAdmin() {
+        this.mudarCargo("Programador");
     }
 }
-const user1 = new Admin("Yan", "teste@teste.com", "Programador", 1);
-user1.cargo = "Pintor";
-user1.mudarCargo();
-console.log(user1);
+const user1 = new Admin(8, "Yan", "teste@teste.com", "Programador", 1);
+// user1.id;
+user1.acessarAdmin();
--- classes/src/heranca.ts
+++ classes/src/heranca.ts
@@ -5,39 +5,54 @@
 
 /*
     4 MODIFICADORES: Public, Private, Protected, readonly
+
+    >> Protected: Atributos e métodos protegidos podem ser acessados ou alterados por meio da 
+     classe em que foram criados e por meio das classes que forem filhas (que extende a classe pai).
+
+    >> Private: Podem ser acessados ou alterados apenas por meio da classe em que foram criados,
+     ou seja ele nao pode ser acessado ou modificado fora da classe em que foi criado.
+
+    >> Readonly: (apenas para atributos) Podemos apenas ler e ver esse atributo porem 
+     nao podemos alterar ele.
 */
 
 class Usuario {
-    public nome: string;
-    public email: string;
+    protected id: number; // Podemos acessar somente na classe usuario ou classe que extends a classe usuario (classes filhas)
+    nome: string;
+    email: string;
 
-    constructor(nome: string, email: string) {
+    constructor(id: number, nome: string, email: string) {
+        this.id = id;
         this.nome = nome;
         this.email = email;
     }
 }
 
 class Admin extends Usuario {
-    public cargo: string;
-    public nivel: number;
+    cargo: string;
+    nivel: number;
 
-    constructor(nome: string, email: string, cargo: string, nivel: number) {
+    constructor(id: number, nome: string, email: string, cargo: string, nivel: number) {
         // Chamando o construtor da classe pai
-        super(nome,email)
+        super(id, nome, email)
 
         this.cargo = cargo;
         this.nivel = nivel
     }
 
-    public mudarCargo(): void {
-        console.log("Alterando cargo")
+    protected mudarCargo(cargo: string): void {
+        console.log("Alterando cargo para:", cargo);
+        console.group("Id do usuário:", this.id);
+    }
+
+    acessarAdmin() {
+        this.mudarCargo("Programador")
     }
 }
 
-const user1 = new Admin("Yan", "teste@teste.com", "Programador", 1)
+const user1 = new Admin(8, "Yan", "teste@teste.com", "Programador", 1)
 
-user1.cargo = "Pintor"
+// user1.id;
 
-user1.mudarCargo();
 
-console.log(user1)
+user1.acessarAdmin();
